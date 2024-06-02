3e84d3203997ff61caf7264e1f5e64d3b307bfda
Thomas Cansino <166280962+thomascansino@users.noreply.github.com>
2024-06-02 15:22:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const convertBtn=document.getElementById('convert-btn');
+const numberInput=document.getElementById('number');
+const output=document.getElementById('output');
+const convertToRoman=(num)=> {
+  const romanNumerals = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' }
+  ];
+
+  let result = '';
+
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (num >= romanNumerals[i].value) {
+      result += romanNumerals[i].numeral;
+      num -= romanNumerals[i].value;
+    }
+  }
+
+  return result;
+}
+
+convertBtn.addEventListener('click',(e)=>{
+  const inputValue=numberInput.value;
+  if(inputValue===''||inputValue.indexOf('.')!==-1){
+    output.textContent='Please enter a valid number';
+  }
+  else if(inputValue<=0){
+      output.textContent='Please enter a number greater than or equal to 1';
+    }
+  else if(inputValue>=4000){
+      output.textContent='Please enter a number less than or equal to 3999';
+    }
+  else{
+    output.textContent=convertToRoman(inputValue);
+  }
+  }
+);
+
+numberInput.addEventListener("keydown", (e) => { 
+  if (e.key === "Enter") {
+    convertBtn.dispatchEvent(new Event('click'));
+  }
+});
